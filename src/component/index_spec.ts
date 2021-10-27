8e30c58e5b633caa8462f6d40d7332e5bf3a5c6a
Xucong Zhan <zhanxucong@gmail.com>
2021-10-26 21:44:46 -0400

    fix: minor component tests clean up to align with the convention

--- src/component/index_spec.ts
+++ src/component/index_spec.ts
@@ -154,24 +154,7 @@ describe('@ngneat/aim Component Schematic', () => {
     expect(tsContent).toMatch(/encapsulation: ViewEncapsulation.None/);
   });
 
-  it('should not create a flat component', async () => {
-    const options = { ...defaultOptions, flat: false };
-
-    const tree = await schematicRunner
-      .runSchematicAsync('component', options, appTree)
-      .toPromise();
-    const files = tree.files;
-    expect(files).toEqual(
-      jasmine.arrayContaining([
-        '/projects/bar/src/app/foo/foo.component.css',
-        '/projects/bar/src/app/foo/foo.component.html',
-        '/projects/bar/src/app/foo/foo.component.spec.ts',
-        '/projects/bar/src/app/foo/foo.component.ts',
-      ])
-    );
-  });
-
-  it('should create a flat component', async () => {
+  it('should respect the flat option', async () => {
     const options = { ...defaultOptions, flat: true };
     const tree = await schematicRunner
       .runSchematicAsync('component', options, appTree)
